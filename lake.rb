558798c20fcf777a9afd11405f8860c426fd8591
Alexander Randa <randa.alex@active.by>
2013-06-08 11:48:29 +0300

    init commit

---
+++ lake.rb
@@ -0,0 +1,40 @@
+module Flying
+  def fly; 'flying' end
+end
+module Quacking
+  def quack; 'quacking' end
+end
+module Swimming
+  def swim; 'swimming' end
+end
+
+class Duck
+  def fly; "can't fly" end
+  def swim; "can't swim" end
+  def quack; "can't quack" end
+end
+
+class RealDuck < Duck
+  include Quacking
+  include Swimming
+  include Flying
+end
+class RubberDuck < Duck
+  include Quacking
+  include Swimming
+end
+class WoodDuck < Duck
+  include Swimming
+end
+
+ducks ||= []
+ducks << RealDuck.new
+ducks << RubberDuck.new
+ducks << WoodDuck.new
+
+ducks.each do |duck|
+  puts "#{duck.class} #{duck.swim}"
+  puts "#{duck.class} #{duck.quack}"
+  puts "#{duck.class} #{duck.fly}"
+
+end
